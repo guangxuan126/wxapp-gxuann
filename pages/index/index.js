0a0a0725cd559846fef0551b7b73570f545c94c7
GuangXuan <tlfggx@gmail.com>
2019-09-07 23:40:07 +0800

    解决postListRequest异步延迟的问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,25 +4,49 @@ const app = getApp()
 import Toast from '../../ui/vantui/toast/toast';
 Page({
   data: {
-    hidden: 'visibility: hidden',
     posts: null,
+    height: ''
   },
   onLoad: function(options) {
-    this.getPostList();
+    this.getScrollHeight();
+    this.getPostsList();
   },
-  getPostList() {
+  getPostsList() {
     let posts = this.data.posts;
-    let hidden = this.data.hidden;
     Toast.loading({
       forbidClick: true,
       message: '加载中...'
     });
-    app.postListRequest().then(res => {
-      Toast.clear;
+    if (app.globalData.posts) {
       this.setData({
-        posts: res.data,
-        hidden: ''
+        posts: app.globalData.posts
       })
+    } else {
+      app.resPostsReadyCallback = res => {
+        this.setData({
+          posts: app.globalData.posts
+        })
+      }
+    }
+  },
+  getScrollHeight() {
+    var that = this;
+    wx.getSystemInfo({
+      success: res => {
+        let cHeight = res.windowHeight;
+        let cWidth = res.windowWidth;
+        let ratio = 750 / cWidth;
+        let height = cHeight * ratio - 330;
+        that.setData({
+          height: height
+        })
+      },
     })
+  },
+  setPostsList() {
+    let posts = this.data.posts;
+    if (!posts) {
+
+    }
   }
 })
