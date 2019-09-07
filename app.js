1144e4a9ebeb8fb99d9d57e9c4364186786af1f1
GuangXuan <tlfggx@gmail.com>
2019-09-07 23:39:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,22 @@ App({
     this.postListRequest();
   },
   postListRequest() {
-    var that = this;
-    return new Promise((resolve, reject) => {
-      let reqUrl = this.globalData.reqUrl;
-      let ghostKey = this.globalData.ghostKey;
+    let reqUrl = this.globalData.reqUrl;
+    let ghostKey = this.globalData.ghostKey;
 
-      wx.request({
-        url: reqUrl + 'posts' + ghostKey,
-        header: {
-          'Content-Type': 'application/json'
-        },
-        success: res => {
-          let resPosts = res.data;
-          that.globalData.posts = resPosts;
-          resolve(res);
-        },
-        fail: () => {
-          reject('error')
+    wx.request({
+      url: reqUrl + 'posts' + ghostKey,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: res => {
+        this.globalData.posts = res.data;
+        if (this.resPostsReadyCallback) {
+          this.resPostsReadyCallback(res)
         }
-      })
+      }
     })
+
   },
   globalData: {
     reqUrl: "https://blog.gxuann.cn/ghost/api/v2/content/",
