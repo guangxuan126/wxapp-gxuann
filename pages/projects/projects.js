0b4afa00687fa807e273933cf7296055bde68a2d
GuangXuan <tlfggx@gmail.com>
2019-09-14 12:33:24 +0800

    项目页 完成

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -1,18 +1,43 @@
 // pages/projects/projects.js
+const app = getApp();
+import Notify from '../../ui/vantui/notify/notify';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bgUrl: 'https://oss.gxuann.cn/wxapp-gxuann/project-cover.png'
   },
-
+  getProjects(){
+    let reqUrl = app.globalData.reqUrl;
+    let ghostKey = app.globalData.ghostKey;
+    wx.request({
+      url: reqUrl + 'pages/5d6889f8f402af74f5bcc599/' + ghostKey,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: (res)=> {
+        let rs = res.data.pages[0];
+        let data = app.towxml.toJson(rs.html, 'html');
+        this.setData({
+          article: data
+        })
+        console.log(rs)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getProjects();
+    Notify({
+      text: '完整内容，请访问 blog.gxuann.cn/projects',
+      duration: 10000,
+      selector: '#custom-selector',
+      backgroundColor: '#de3f33'
+    });
   },
 
   /**
